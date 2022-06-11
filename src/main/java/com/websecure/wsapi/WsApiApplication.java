47addaf8614db231bc4b50b126a17c40f494dd31
Bisoye Mayegun <bmayegun@gmail.com>
2022-06-11 18:43:48 +0100

    Project initialisation
    Added gitignore

---
+++ src/main/java/com/websecure/wsapi/WsApiApplication.java
@@ -0,0 +1,13 @@
+package com.websecure.wsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsApiApplication.class, args);
+    }
+
+}
